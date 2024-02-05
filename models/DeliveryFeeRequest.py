1556f71f2d533e679da2a1c2bf8af83787bbda2a
unknown <tomppa.miettinen1@gmail.com>
2024-02-05 12:41:15 +0200

    add config validation

--- models/DeliveryFeeRequest.py
+++ models/DeliveryFeeRequest.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel, conint
+from pydantic import BaseModel, Field
+from typing import Annotated
 from datetime import datetime
 
 
 class DeliveryFeeRequest(BaseModel):
-    cart_value: conint(gt=0)
-    delivery_distance: conint(gt=0)
-    number_of_items: conint(gt=0)
+    cart_value: Annotated[int, Field(gt=0)]
+    delivery_distance: Annotated[int, Field(gt=0)]
+    number_of_items: Annotated[int, Field(gt=0)]
     time: datetime
--- services/delivery_fee_service.py
+++ services/delivery_fee_service.py
@@ -1,62 +1,73 @@
 from models.DeliveryFeeRequest import DeliveryFeeRequest
-from pydantic import validate_call
+from pydantic import validate_call, BaseModel, validator, Field
 from datetime import datetime
+from typing import Annotated, Any, Dict
 import math
 
-BASE_FEE = 200
-ADDITIONAL_FEE = 100
 
-BASE_DISTANCE = 1000
-ADDITIONAL_DISTANCE = 500
+class DeliveryFeeConfig(BaseModel):
+    BASE_FEE: Annotated[int, Field(gt=0, validate_default=True)] = 200
+    ADDITIONAL_FEE: Annotated[int, Field(gt=0, validate_default=True)] = 100
+    BASE_DISTANCE: Annotated[int, Field(gt=0, validate_default=True)] = 1000
+    ADDITIONAL_DISTANCE: Annotated[int, Field(gt=0, validate_default=True)] = 500
+    BULK_ITEMS_THRESHOLD_1: Annotated[int, Field(gt=0, validate_default=True)] = 4
+    BULK_ITEMS_THRESHOLD_2: Annotated[int, Field(gt=0, validate_default=True)] = 12
+    BULK_ITEMS_THRESHOLD_1_FEE: Annotated[int, Field(gt=0, validate_default=True)] = 50
+    BULK_ITEMS_THRESHOLD_2_FEE: Annotated[int, Field(gt=0, validate_default=True)] = 120
+    MAX_FEE: Annotated[int, Field(gt=0, validate_default=True)] = 1500
+    CART_VALUE_THRESHOLD_FOR_SURCHARGE: Annotated[
+        int, Field(gt=0, validate_default=True)
+    ] = 1000
+    RUSH_HOUR_DAY_OF_WEEK: Annotated[int, Field(ge=0, lt=7, validate_default=True)] = 4
+    RUSH_HOUR_START_HOUR: Annotated[int, Field(ge=0, lt=24, validate_default=True)] = 15
+    RUSH_HOUR_END_HOUR: Annotated[int, Field(ge=0, lt=24, validate_default=True)] = 19
+    RUSH_HOUR_FEE_MULTIPLIER: Annotated[float, Field(gt=0, validate_default=True)] = 1.2
 
-BULK_ITEMS_THRESHOLD_1 = 4
-BULK_ITEMS_THRESHOLD_2 = 12
+    @validator("RUSH_HOUR_END_HOUR")
+    def validate_rush_hours(cls, value: int, values: Dict[str, Any]):
+        if "RUSH_HOUR_START_HOUR" in values and value <= values["RUSH_HOUR_START_HOUR"]:
+            raise ValueError(
+                "RUSH_HOUR_END_HOUR must be greater than RUSH_HOUR_START_HOUR"
+            )
+        return value
 
-BULK_ITEMS_THRESHOLD_1_FEE = 50
-BULK_ITEMS_THRESHOLD_2_FEE = 120
 
-MAX_FEE = 1500
-CART_VALUE_THRESHOLD_FOR_SURCHARGE = 1000
-
-RUSH_HOUR_DAY_OF_WEEK = 4
-RUSH_HOUR_START_HOUR = 15
-RUSH_HOUR_END_HOUR = 19
-RUSH_HOUR_FEE_MULTIPLIER = 1.2
+config = DeliveryFeeConfig()
 
 
 @validate_call
 def _is_rush_hour(time: datetime):
     return (
-        time.weekday() == RUSH_HOUR_DAY_OF_WEEK
-        and RUSH_HOUR_START_HOUR <= time.hour < RUSH_HOUR_END_HOUR
+        time.weekday() == config.RUSH_HOUR_DAY_OF_WEEK
+        and config.RUSH_HOUR_START_HOUR <= time.hour < config.RUSH_HOUR_END_HOUR
     )
 
 
 @validate_call
-def _get_small_order_surcharge(cart_value: int) -> int:
-    return max(CART_VALUE_THRESHOLD_FOR_SURCHARGE - cart_value, 0)
+def _get_small_order_surcharge(cart_value: Annotated[float, Field(gt=0)]):
+    return max(config.CART_VALUE_THRESHOLD_FOR_SURCHARGE - cart_value, 0)
 
 
 @validate_call
-def _get_distance_fee(delivery_distance: int) -> int:
-    if delivery_distance <= BASE_DISTANCE:
-        return BASE_FEE
+def _get_distance_fee(delivery_distance: Annotated[int, Field(gt=0)]):
+    if delivery_distance <= config.BASE_DISTANCE:
+        return config.BASE_FEE
 
     distance_over_base = (
-        math.ceil(delivery_distance - BASE_DISTANCE) / ADDITIONAL_DISTANCE
+        math.ceil(delivery_distance - config.BASE_DISTANCE) / config.ADDITIONAL_DISTANCE
     )
-    return BASE_FEE + (distance_over_base * ADDITIONAL_FEE)
+    return int(config.BASE_FEE + (distance_over_base * config.ADDITIONAL_FEE))
 
 
 @validate_call
-def _get_bulk_fee(number_of_items: int) -> int:
+def _get_bulk_fee(number_of_items: Annotated[int, Field(gt=0)]):
     fee = 0
-    if number_of_items > BULK_ITEMS_THRESHOLD_1:
-        items_over_threshold_1 = number_of_items - BULK_ITEMS_THRESHOLD_1
-        fee = items_over_threshold_1 * BULK_ITEMS_THRESHOLD_1_FEE
+    if number_of_items > config.BULK_ITEMS_THRESHOLD_1:
+        items_over_threshold_1 = number_of_items - config.BULK_ITEMS_THRESHOLD_1
+        fee = items_over_threshold_1 * config.BULK_ITEMS_THRESHOLD_1_FEE
 
-    if number_of_items > BULK_ITEMS_THRESHOLD_2:
-        fee += BULK_ITEMS_THRESHOLD_2_FEE
+    if number_of_items > config.BULK_ITEMS_THRESHOLD_2:
+        fee += config.BULK_ITEMS_THRESHOLD_2_FEE
 
     return fee
 
@@ -70,6 +81,6 @@ def get_total_delivery_fee(request: DeliveryFeeRequest):
     fee = small_order_surcharge + distance_fee + bulk_fee
 
     if _is_rush_hour(request.time):
-        fee *= RUSH_HOUR_FEE_MULTIPLIER
+        fee *= config.RUSH_HOUR_FEE_MULTIPLIER
 
-    return min(fee, MAX_FEE)
+    return round(min(fee, config.MAX_FEE), 2)
